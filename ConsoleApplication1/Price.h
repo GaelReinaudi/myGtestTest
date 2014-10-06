6907c7b298805a885cb3aa7468fadb81de443450
Gael Reinaudi <gael.reinaudi@gmail.com>
2014-10-06 00:54:11 -0400

    few more tests

--- ConsoleApplication1/Price.h
+++ ConsoleApplication1/Price.h
@@ -2,20 +2,31 @@
 class Price
 {
 public:
-	Price(double dblVal)
+	Price()
 	{
-		double d = dblVal;
+	}
+	explicit Price(double dblVal)
+	{
+			double d = dblVal;
 		d += 2048.0 + 1e-10;
 		d *= 65536.0;
 		i = int(d);
 		decomp.floor -= 2048;
+		//decomp.frac += 1;
+		//decomp.frac &= 0xFFFE;
 	}
 	~Price()
 	{
 	}
 
 public:
-
+	Price operator-(const Price & p2)
+	{
+		Price pr;
+		pr.i = i - p2.i;
+//		decomp.frac &= 0xFFFE;
+		return pr;
+	}
 
 public:
 	union {
--- unittestPrice/unittestPrice.cpp
+++ unittestPrice/unittestPrice.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-
+#include <map>
 //int _tmain(int argc, _TCHAR* argv[])
 //{
 //	return 0;
@@ -12,19 +12,28 @@
 #include "gtest/gtest.h"
 #include "Price.h"
 
+TEST(testMath, zero)
+{
+	Price p(0.0);
+	EXPECT_EQ(p.decomp.floor, 0);
+	EXPECT_EQ(p.decomp.frac, 0);
+}
+
 TEST(testMath, decompositionThousand)
 {
-	for (double d = -1000.0; d <= 1000.0; d += 0.001)
+	for (double d = -1000.0; d <= 1000.0 + 1e-9; d += 0.001)
 	{
 		Price p(d);
 		EXPECT_EQ(p.decomp.floor, floor(d + 1e-10));
-		EXPECT_EQ(p.decomp.frac,   unsigned short((d + 1e-10 - p.decomp.floor) * (1 << 16)));
+		unsigned short fr = unsigned short((d + 1e-10 - p.decomp.floor) * (1 << 16));
+//		fr &= 0xFFFE;
+		EXPECT_EQ(p.decomp.frac, fr);
 	}
 }
 
 TEST(testMath, decompositionMillionNeg)
 {
-	for (double d = -1000.0; d <= -998.0; d += 0.000001)
+	for (double d = -1000.0; d <= -998.0 + 1e-9; d += 0.000001)
 	{
 		Price p(d);
 		EXPECT_EQ(p.decomp.floor, floor(d + 1e-9));
@@ -32,7 +41,7 @@ TEST(testMath, decompositionMillionNeg)
 }
 TEST(testMath, decompositionMillionPos)
 {
-	for (double d = 1000.0; d <= 1002.0; d += 0.000001)
+	for (double d = 1000.0; d <= 1002.0 + 1e-9; d += 0.000001)
 	{
 		Price p(d);
 		EXPECT_EQ(p.decomp.floor, floor(d + 1e-9));
@@ -40,29 +49,52 @@ TEST(testMath, decompositionMillionPos)
 }
 TEST(testMath, decompositionMillionNull)
 {
-	for (double d = -1.0; d <= 1.0; d += 0.000001)
+	for (double d = -1.0; d <= 1.0 + 1e-9; d += 0.000001)
 	{
 		Price p(d);
 		EXPECT_EQ(p.decomp.floor, floor(d + 1e-9));
 	}
 }
 
-//TEST(testMath, myCubeTest)
-//{
-//	Price p1(121.2500);
-//	Price p2(122.7500);
-//
-//	ps = p1 + p2;
-//	EXPECT_EQ(ps.toDouble(), 244.0000);
-//}
-
-
-/* Test No. 2
-TEST(testCalc, MySecondTest)
+TEST(testMath, fracBins)
 {
-...
+	std::map<unsigned short, int> allFracBins;
+	// for the 0 that is counted once more
+	--allFracBins[0];
+	int subVal = 1000;
+	for (int i = -1000 * subVal; i <= 1000 * subVal; ++i)
+	{
+		double d = i;
+		d /= double(subVal);
+		Price p(d);
+		++allFracBins[p.decomp.frac];
+	}
+	for (const auto & p : allFracBins)
+	{
+		EXPECT_EQ(p.second, 2 * subVal);
+	}
 }
-*/
+
+TEST(testMath, substraction_100)
+{
+	int subVal = 100;
+	for (int i = -1000 * subVal; i <= 1000 * subVal; ++i)
+	{
+		double d = i;
+		d /= double(subVal);
+		Price p(d);
+		for (int j = -1000 * subVal; j <= 1000 * subVal; ++j)
+		{
+			double d2 = j;
+			d2 /= double(subVal);
+			Price p2(d2);
+			double sub = d - d2;
+			Price psub(sub);
+			//EXPECT_EQ(psub.i, (p - p2).i);
+		}
+	}
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
